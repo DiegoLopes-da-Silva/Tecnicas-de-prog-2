7fb9766e7974acf1eec07c82b8df20aa676d1a37
Diego Lopes da Silva <diego.silva.09lopes@gmail.com>
2026-05-25 11:01:31 -0300

    Adicionando conteudo da aula

---
+++ 05-25/Frontend/src/App.vue
@@ -0,0 +1,22 @@
+-- Ta incompleto, tem mais coisa depois.
+
+
+<template>
+<div>
+<h1>Minhas tarefas</h1>
+<input v-model="titulo.tarefa" placeholder="Digite o titulo da tarefa">
+<textarea v-model = "descricaoTarefa" placeholder = "Digite a descricao da tarefa"></textarea>
+<button @click = "adicionarTarefa">Adicionar</button>
+<ul>
+  <li v-for = "item in tarefas":key = "item.id">{{ item.titulo }} - {{ item.concluida ? "Concluida" : "Aguardando" }}</li>
+</ul>
+</div>
+</template>
+<script setup>
+import {ref, nMounted} from "vue";
+import axios from "axios";
+const tarefas = ref([]);
+const tituloTarefa = ref('');
+const descricaoTarefa = ref('');
+const API_URL = "http://localhost:5081/api/tarefas";
+</script>
